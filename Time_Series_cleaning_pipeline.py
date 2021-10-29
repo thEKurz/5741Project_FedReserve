ea8dc23ab4872ea7a7f0a84f5523241389dcb9fc
Evan Kurzman <evankurzman@gmail.com>
2021-10-29 01:45:42 -0400

    time series pipeline

---
+++ Time_Series_cleaning_pipeline.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+import pandas as pd
+import glob
+files = glob.glob("C:/Users/evank/OneDrive/Documents/Academics/Cornell/Fall 2021/5741 Learning With Big Messy Data/Project/5741Project_FedReserve/CSV_Data/*.csv")
+
+dflist=[]
+
+#make list of dataframes
+for f in files:
+    csv = pd.read_csv(f)
+    dflist.append(csv)
+    
+#make our dataframe 
+Numerical_data_df= pd.DataFrame()
+
+for n in range(0,len(dflist)):
+    #standardize dates and make datetime index
+    dflist[n].iloc[:,0] = dflist[n].iloc[:,0].str.replace('/','-')
+    dflist[n].rename(columns={ dflist[n].columns[0]: "DATE" }, inplace = True)
+    dflist[n]['DATE']=pd.to_datetime(dflist[n]['DATE'])
+    dflist[n].set_index(dflist[n].iloc[:,0], inplace=True)
+    dflist[n]=dflist[n].drop(dflist[n].columns[[0]], axis=1)
+    
+    #remove rows with non-numeric data
+    dflist[n]=dflist[n][pd.to_numeric(dflist[n].iloc[:,0], errors='coerce').notnull()]
+    
+    #merge frame without losing any data
+    Numerical_data_df= Numerical_data_df.merge(dflist[n], how='outer', left_index=True, right_index=True)
+
+Numerical_data_df.fillna(method='ffill',inplace=True)
+Numerical_data_df.fillna(0,inplace=True)
+Freq_data_df=Numerical_data_df.groupby(Numerical_data_df.index.to_period('M')).nth(0)
