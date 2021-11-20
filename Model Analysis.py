aaa37f853d4e84a7daf7934120f2a052d4455bfb
Evan Kurzman <evankurzman@gmail.com>
2021-11-20 14:11:41 -0500

    update model analysis

--- Model Analysis.py
+++ Model Analysis.py
@@ -8,6 +8,18 @@ Created on Sat Nov 20 12:18:24 2021
 import pandas as pd
 import sklearn as sk
 
+#bring in dataframes
+Text_DF=pd.read_csv('text_sent_1.csv',index_col='Date',parse_dates=['Date'])
+I_df=pd.read_csv('Independant_Numeric_Variables.csv',index_col='DATE',parse_dates=['DATE'])
+D_df=pd.read_csv('Dependant_Numeric_Variables.csv',index_col='DATE',parse_dates=['DATE'])
+
+
+#Text_DF.set_index(pd.to_datetime(Text_DF.index), inplace=True)
+
+I_df[Text_DF.columns]=Text_DF[Text_DF.columns]
+I_df.fillna(method='ffill',inplace=True)
+I_df.fillna(0,inplace=True)
+
 def TS_pipe(X,window_size=24):
     df=(X/(X.max()-X.min()))
     for x in df.columns:
@@ -17,6 +29,11 @@ def TS_pipe(X,window_size=24):
         shifted = df_1.shift(window)
         df=df.join(shifted.rename(columns=lambda x: x+ "_" + str(window) + "_lag"))
     return df
+
+I_df_norm=TS_pipe(I_df)
+
+I_df_post_2003=I_df_norm[~(I_df_norm.index<'2003-01-01')]
+D_df_post_2003=D_df[~(D_df.index<'2003-01-01')]
 
 D_df_norm= (D_df_post_2003/(D_df_post_2003.max()-D_df_post_2003.min()))
 
