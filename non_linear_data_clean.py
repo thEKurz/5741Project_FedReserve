5ce6c4ce73782000dd808b0ba90ca6c9ab5df5a4
skysfan0620 <kelvinsun0620@gmail.com>
2021-12-01 16:16:38 -0500

    ridge regression

--- non_linear_data_clean.py
+++ non_linear_data_clean.py
@@ -50,6 +50,26 @@ D_df_post_2003=D_df[~(D_df.index<'2003-01-01')]
 I_df_post_2003.to_csv("Independent_post2003.csv", index = True)
 D_df_post_2003.to_csv("Dependent_post2003.csv", index = True)
 
+# Data with lags
 I_df_pipe = TS_pipe(I_df_transform, 24)
 I_df_pipe =I_df_pipe[~(I_df_pipe.index<'2003-01-01')]
 I_df_pipe.to_csv("Independent_post2003_withlag.csv", index = True) 
+
+# Data with moving average buckets
+def TS_lag_buckets(X,window_size=24,lag_buckets=4):
+    bucket_size=int(window_size/lag_buckets)
+    df=X
+    df_1=df
+    for window in np.arange(1, window_size+1,bucket_size):
+        shifted = df_1.shift(window)
+        for s in range(window,window+bucket_size):
+            shifted += df_1.shift(s)
+        shifted=shifted/bucket_size
+        df=df.join(shifted.rename(columns=lambda x: x+ "_lag_" + str(window)+ "_"+str(window+bucket_size-1)))
+    for x in df_1.columns:
+        df[str(x) + '_MA_' + str(window_size)] = df[x].rolling(window=window_size).mean().copy()
+    return df
+
+I_df_buck = TS_lag_buckets(I_df_transform, 24, 4)
+I_df_buck =I_df_buck[~(I_df_buck.index<'2003-01-01')]
+I_df_buck.to_csv("Independent_post2003_buckets.csv", index = True) 
---
+++ ridge.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import sklearn.model_selection
+import matplotlib.pyplot as plt
+
+I_df = pd.read_csv("Independent_post2003.csv", index_col = "DATE", parse_dates = ["DATE"])
+D_df = pd.read_csv("Dependent_post2003.csv", index_col = "DATE", parse_dates = ["DATE"])
+
+def perform_ridge(I_df, D_df):
+    # Get k-fold cross validation spilt
+    # k = 5
+    kf = sklearn.model_selection.KFold(n_splits=5)
+    kf.get_n_splits(I_df)
+    
+    # Prepare for ridge regression
+    from sklearn.linear_model import Ridge
+    
+    
+    regularizer_coef = [0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1]
+    avg_score_table = pd.DataFrame(index = D_df.columns, columns = regularizer_coef)
+    
+    for d in range(D_df.shape[1]):
+        y = D_df.iloc[:,d]
+        
+        for i in regularizer_coef:
+            rid = Ridge(alpha= i, 
+                fit_intercept = True, 
+                normalize = False, 
+                tol=0.001)
+            
+            n = 0
+            score_table = np.empty(5)
+            for train_index, test_index in kf.split(I_df):
+                X_train = np.asarray(I_df.iloc[train_index,:])
+                X_test = np.asarray(I_df.iloc[test_index,:])
+                y_train = np.asarray(y.iloc[train_index])
+                y_test = np.asarray(y.iloc[test_index])
+                        
+                m1 = rid.fit(X_train, y_train)
+                score_table[n] = m1.score(X_test, y_test)
+                n += 1
+            
+            avg_score_table.loc[D_df.columns[d],i] = np.mean(score_table)
+    return avg_score_table
+
+avg_normal = perform_ridge(I_df, D_df)
+
+I_df = pd.read_csv("Independent_post2003_withlag.csv", index_col = "DATE", parse_dates = ["DATE"])
+avg_lags = perform_ridge(I_df, D_df)
+
+I_df = pd.read_csv("Independent_post2003_buckets.csv", index_col = "DATE", parse_dates = ["DATE"])
+avg_buckets = perform_ridge(I_df, D_df)
+
+for d in range(D_df.shape[1]):
+    plt.plot(avg_normal.columns, avg_normal.iloc[d,:], label = avg_normal.index[d])
+plt.legend(bbox_to_anchor=(1, 1))
+plt.title("Out of sample prediction score for ridge regression")
+plt.show()
+
+for d in range(D_df.shape[1]):
+    plt.plot(avg_lags.columns, avg_lags.iloc[d,:], label = avg_lags.index[d])
+plt.legend(bbox_to_anchor=(1, 1))
+plt.title("Out of sample prediction score for ridge regression with lags")
+plt.show()
+
+for d in range(D_df.shape[1]):
+    plt.plot(avg_buckets.columns, avg_buckets.iloc[d,:], label = avg_buckets.index[d])
+plt.legend(bbox_to_anchor=(1, 1))
+plt.title("Out of sample prediction score for ridge regression with moving average buckets")
+plt.show()
